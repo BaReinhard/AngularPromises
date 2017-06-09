045fae2791a7b520a1dfc9df784f04ab642c64c3
Brett Reinhard <brettreinhard@gmail.com>
2017-06-08 20:20:45 -0700

    Create HomeController.js

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,7 +1,7 @@
 app.controller('HomeController',[ '$scope','$http','$q','$timeout',function($scope,$http,$q,$timeout){
 // Practicing Promises
     var promises = [];
-    function AsyncCallBack(msec,text) {
+    function AsyncFunction(msec,text) {
         var defer = $q.defer();
         $timeout(function () {
             defer.resolve();
@@ -16,24 +16,24 @@ app.controller('HomeController',[ '$scope','$http','$q','$timeout',function($sco
     
 
     // Fourth to Return
-    promises.push(AsyncCallBack(3000, "First CallBack Done"));
+    promises.push(AsyncFunction(3000, "First CallBack Done"));
 
     // Third to Return
-    promises.push(AsyncCallBack(2500, "Second CallBack Done"));
+    promises.push(AsyncFunction(2500, "Second CallBack Done"));
 
     // Second to Return
-    promises.push(AsyncCallBack(400, "Third Callback Done").then(function(){
+    promises.push(AsyncFunction(400, "Third Callback Done").then(function(){
         PrintWhenDone("Creating a new Promise after Third CallBack");
-        promises.push(AsyncCallBack(4000, "Call back created in Third Call Back Then Function").then(PrintWhenDone.bind(null, "is this confusing enough yet")));
+        promises.push(AsyncFunction(4000, "Call back created in Third Call Back Then Function").then(PrintWhenDone.bind(null, "is this confusing enough yet")));
     }));
     // Fifth to Return
-    promises.push(AsyncCallBack(10000, "Fourth Callback Done").then(PrintWhenDone.bind(null,"Fourth Call Back then Function")));
+    promises.push(AsyncFunction(10000, "Fourth Callback Done").then(PrintWhenDone.bind(null,"Fourth Call Back then Function")));
 
     // First to Return
-    promises.push(AsyncCallBack(100, "Fifth Callback Done"));
+    promises.push(AsyncFunction(100, "Fifth Callback Done"));
 
     // Last to Return
-    promises.push(AsyncCallBack(11000, "Sixth Callback Done"));
+    promises.push(AsyncFunction(11000, "Sixth Callback Done"));
 
     // When all Promises have resolved
     $q.all(promises).then(function () {
